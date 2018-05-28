08076e13dc54a6f0788bf9ad3cfa22e3c3f4d388
A. N. TANVIR <35560149+antanvir@users.noreply.github.com>
2018-05-29 02:00:23 +0600

    Add files via upload

--- SPL-1/src/Main.java
+++ SPL-1/src/Main.java
@@ -1,21 +1,30 @@
+
 package forspl;
 
+import java.awt.AWTEventMulticaster;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
 
 public class Main extends Thread{
     
    //public static void main(String[] args)throws FileNotFoundException{
-    @Override
+   
     public void run(){
         String tokens[] = {"int ","double ","float ","char ","boolean ","String "};
         
         Scanner in = null;
         try {
-            in = new Scanner(new File("C:\\Users\\antan\\eclipse-workspace\\ForSpl\\ReadIt.txt"));
+            in = new Scanner(new File("D:\\Users\\My-PC\\Documents\\NetBeansProjects\\ForSPL\\ReadIt2.txt"));
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -37,6 +46,18 @@ public class Main extends Thread{
             }
         }
         obj1.PrintVariables();
-        obj2.PrintVariables();
+        //obj2.PrintVariables();
+        
+        JFrame f=new JFrame();
+        //UIManager.put("OptionPane.minimumSize",new Dimension(250,90)); 
+        if(JOptionPane.showConfirmDialog(f,"Wish to See Varible Names?")==JOptionPane.YES_OPTION){ 
+           
+            ShowVariable info =new ShowVariable();
+            ShowVariable.main(null);
+            
+        }  
+        
     }
+
+    
 }
